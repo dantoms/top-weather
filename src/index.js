017b1cb3c29788dae9e4ebf90e8729e3bc0f3135
Dan Toms <157254317+pynmash@users.noreply.github.com>
2026-05-18 23:50:37 +0100

    Display fetched data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const locInput = document.querySelector(".location-input");
 const unitSwitch = document.querySelector(".unit-toggle");
 let unit = "us";
 
+const unitSymbols = {
+  uk: "℃",
+  us: "℉",
+};
+
 async function getData(location, unit) {
   const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=${unit}&key=A3TFBZ58P6XLMGNRTQAYKLGVA&contentType=json`,
@@ -21,9 +26,53 @@ function switchUnits() {
   }
 }
 
+function displayData(data) {
+  const weatherDetail = document.querySelector(".weather-detail");
+  weatherDetail.innerHTML = "";
+
+  const conditions = document.createElement("p");
+  conditions.textContent = data.currentConditions.conditions;
+  conditions.classList.add("conditions");
+
+  const location = document.createElement("h2");
+  location.textContent = data.resolvedAddress;
+  location.classList.add("location");
+
+  const tempMore = document.createElement("div");
+  tempMore.classList.add("temp-more");
+
+  const temp = document.createElement("p");
+  temp.textContent = data.currentConditions.temp;
+  temp.style.setProperty("--unit-symbol", `"${unitSymbols[unit]}"`);
+  temp.classList.add("temp");
+
+  const more = document.createElement("div");
+  more.classList.add("more");
+
+  const feelslike = document.createElement("p");
+  feelslike.textContent = `Feel: ${data.currentConditions.feelslike}`;
+  feelslike.style.setProperty("--unit-symbol", `"${unitSymbols[unit]}"`);
+  feelslike.classList.add("feelslike");
+
+  const windspeed = document.createElement("p");
+  windspeed.textContent = `Wind Speed: ${data.currentConditions.windspeed}mph`;
+  windspeed.classList.add("windspeed");
+
+  const humidity = document.createElement("p");
+  humidity.textContent = `Humidity: ${data.currentConditions.humidity}%`;
+  humidity.classList.add("humidity");
+
+  more.append(feelslike, windspeed, humidity);
+  tempMore.append(temp, more);
+  weatherDetail.append(conditions, location, tempMore);
+}
+
 function loadData(location) {
   const data = getData(location, unit);
-  data.then((data) => console.log(data));
+  data.then((data) => {
+    console.log(data);
+    displayData(data);
+  });
 }
 
 unitSwitch.addEventListener("click", switchUnits);
